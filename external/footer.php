ba379f784e473042f14437634b162584711203cb
rmit-s3538102-byung-yun <s3538102@student.rmit.edu.au>
2017-10-01 20:22:23 +1100

    Add files via upload

---
+++ external/footer.php
@@ -0,0 +1,55 @@
+<!-- https://demo.tutorialzine.com/2016/10/freebie-5-fantastic-bootstrap-footers/#Footer-with-button-logo -->
+<div class="content">
+</div>
+<footer id="footerContainer">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/footer.css">
+
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-3">
+        <h2 class="logo"><a href="index.php"> Splendr </a></h2>
+        <p> 124 La Trobe St, Melbourne VIC 3000</p>
+        <p>(03) 9925 2000</p>
+      </div>
+
+      <div class="col-sm-2">
+        <h5>Get Started</h5>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Sign Up</a></li>
+          <li><a href="#">Log In</a></li>
+        </ul>
+      </div>
+
+      <div class="col-sm-2">
+        <h5>About Us</h5>
+        <ul>
+          <li><a href="#">Company Information</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+
+      <div class="col-sm-2">
+        <h5>Legal</h5>
+        <ul>
+          <li><a href="#">Terms and Condition</a></li>
+          <li><a href="#">Site Map</a></li>
+        </ul>
+      </div>
+
+      <div class="col-sm-3">
+        <div class="social-networks">
+          <a href="#" class="twitter"><img src="../img/twitter.png"></a>
+          <a href="#" class="facebook"><img src="../img/facebook.png"></a>
+          <a href="#" class="instagram"><img src ="../img/instagram.png"></a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-copyright">
+    <p> RMIT University © Copyright Splendr 2017</p>
+  </div>
+</footer>
---
+++ external/nav.php
@@ -0,0 +1,35 @@
+<?php
+	$db = mysqli_connect("localhost","root","","accounts"); //databsae connection
+	$username = $_SESSION["username"]; //$_SESSION["username"] is the username of the user that is logged in
+
+	$get = "SELECT user_id FROM members WHERE username = '$username'"; // SQL condition grab the logged in user's user id
+	$result = $db->query($get); // execute query
+
+	if ($row = mysqli_fetch_assoc($result)) { // if conditions are met, run the code below
+		$myid = $row["user_id"];
+		$_SESSION["myid"] = $myid;
+	}else{
+		echo "User ID has not been found!";
+	}
+
+	foreach($db->query("SELECT COUNT(message) FROM message WHERE friend_id ='$myid' AND status= '1'") as $row) { // Message Notification System, check for any unread messages to the logged in user
+		$notification = $row['COUNT(message)'];
+	}
+?>
+<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+<link rel="stylesheet" href="../css/nav.css">
+<nav class ="navigation">
+  <ul>
+    <li class="img"><a href="../index/index.php"><img src="../img/Logo.png"></a></li>
+    <li><a class ="link"href="../Preference/Match.php">MATCH & CHAT(<?php echo $notification;?>)</a></li>
+    <li><a class="link" href="../Transaction/cart.php">TRANSACTIONS</a></li>
+    <?php
+			if(isset($_SESSION["loggedin"])){
+	      echo"<li><a class='link' href ='../Profile/profilepage.php?user=$myid'>PROFILE</a></li>";
+	    }else{
+	      echo"<li><a class ='link' href ='../Login/login.php'>LOGIN</a></li>";
+	    }
+		?>
+  </ul>
+</nav>
